98983a4db00ff26db18468d72a9df601e616f8b0
taymuraz15 <tamutamutautau@gmail.com>
2026-06-09 11:49:16 +0300

    Создание формы Sklad.cs (Первая форма)

--- SkladOfProduction/Product.cs
+++ SkladOfProduction/Product.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SkladOfProduction
 {
-    internal class Product
+    public class Product
     {
+        public string Name { get; set; }
+        public int OstatokDney { get; set; }
+        public DateTime DataPostupleniya { get; set; }
+        public Product(string name, int ostatokDney, DateTime dataPostupleniya = default)
+        {
+            Name = name;
+            this.OstatokDney = ostatokDney;
+            this.DataPostupleniya = dataPostupleniya;
+        }
     }
 }
